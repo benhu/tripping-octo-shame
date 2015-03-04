76e5a34da826e8745f93ff7ca6fc1056f6ce8b02
Benjamin <benjamin.humeau@epsi.fr>
2015-03-04 21:27:35 +0100

    Modification d'angularjs

--- angularjs/js/controllers.js
+++ angularjs/js/controllers.js
@@ -1,11 +1,11 @@
 'use strict';
 
 /* Controllers */
-var produitcatControllers = angular.module('produitcatControllers', []);
+var produitcatControllers = angular.module('productApp', []);
 
-produitcatControllers.controller('ProduitListCtrl', ['$scope', '$http',
+produitcatControllers.controller('ProductListCtrl', ['$scope', '$http',
   function ($scope, $http) {
-    $http.get('produits/produits.json').success(function(data) {
+    $http.get('http://127.0.0.1/wsprovider/WSProvider/rest/product').success(function(data) {
       $scope.produits = data;
     });
     $scope.orderProp = 'quantity';
