26969d06def89c111040ccf0b4117452692899d4
boriest <boriest@gmail.com>
2015-03-24 14:57:33 +0100

    blabla

--- WebContent/angularjs/js/app.js
+++ WebContent/angularjs/js/app.js
@@ -2,54 +2,83 @@
 
 var SERVER_URL = 'http://127.0.0.1:8080/WSProvider/rest/';
 
-/* App Module */
-var productApp = angular.module('productApp', [
-  'ngRoute',
-  'ngCookies'
-]);
+angular.module('productApp', [
+    'ngRoute',
+    'ngCookies'
+    ])
 
-productApp.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider
-            .when('/produits', {
-                templateUrl: 'partials/ListProduits.html',
-                controller: 'ProduitListCtrl'
-            })
-            .otherwise({
-                redirectTo: '/produits'
-            });
-    }
-]);
-
-productApp.controller('ProductListCtrl', ['$scope', '$http', '$cookieStore',
-    function ($scope, $http, $cookieStore) {
-        $http.get(SERVER_URL + 'product').success(function(data) {
-            $scope.produits = data;
-        });
-        $scope.orderProp = 'quantity';
-
-        $scope.orderProduct = function (ref, quantity) {
-            console.log(arguments);
-        };
-    }
-]);
-
-productApp.controller('LoginCtrl', ['$scope', '$http', '$cookieStore',
-    function ($scope, $http, $cookieStore) {
-        $scope.handleLogin = function () {
-            var data, headers;
-            data = 'username=' + $scope.username +'&password=' + $scope.password;
-            headers = {'Content-Type': 'application/x-www-form-urlencoded'};
-
-            $http
-                .post(SERVER_URL + 'connect', data, {headers: headers})
-                .success(function (data, status) {
-                    console.log('youpi', data, status);
-                    $cookieStore.put('user_token', data.guid);
+    .config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider
+                .when('/produits', {
+                    templateUrl: 'partials/ListProduits.html',
+                    controller: 'ProduitListCtrl'
                 })
-                .error(function (data, status) {
-                    alert('nope');
+                .otherwise({
+                    redirectTo: '/produits'
                 });
-        };
-    }
-]);
+        }
+    ])
+
+    .controller('ProductListCtrl', ['$scope', '$http', '$cookieStore',
+        function ($scope, $http, $cookieStore) {
+            $http.get(SERVER_URL + 'product').success(function(data) {
+            	data.forEach(function (product) {
+            		product.number = 0;
+            	});
+                $scope.produits = data;
+            });
+            $scope.orderProp = 'quantity';
+            
+            $scope.getProduct = function (ref) {
+            	return $scope.produits.filter(function (product) {
+            		return product.reference == ref;
+            	})[0];
+            };
+
+            $scope.orderProduct = function (ref) {
+            	var product = $scope.getProduct(ref),
+                    params = {
+                        reference: product.reference,
+                        quantity: product.number,
+                        token: $cookieStore.get('user_token')
+                    },
+                    data = [],
+                    headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+            	for (var k in params) data.push(k + '=' + params[k]);
+            	console.log(data.join('&'));
+            	$http
+                    .post(SERVER_URL + 'order', data.join('&'), {headers: headers})
+            		.success(function (data, status) {
+            			product.quantity -= data.quantity;
+            		})
+            		.error(function (data, status) {
+            			if (status == 400) {
+            				alert("Wrong quantity");
+            			} else if (status == 403) {
+            				alert("You must be logged in");
+            			}
+            		});
+            };
+        }
+    ])
+
+    .controller('LoginCtrl', ['$scope', '$http', '$cookieStore',
+        function ($scope, $http, $cookieStore) {
+            $scope.handleLogin = function () {
+                var data, headers;
+                data = 'username=' + $scope.username +'&password=' + $scope.password;
+                headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+
+                $http
+                    .post(SERVER_URL + 'connect', data, {headers: headers})
+                    .success(function (data, status) {
+                        console.log('youpi', data, status);
+                        $cookieStore.put('user_token', data.guid);
+                    })
+                    .error(function (data, status) {
+                        alert('nope');
+                    });
+            };
+        }
+    ]);
--- WebContent/angularjs/js/controllers.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var SERVER_URL = 'http://127.0.0.1:8080/WSProvider/rest/';
-
-/* Controllers */
-var produitcatControllers = angular.module('productApp', ['ngCookies']);
-
