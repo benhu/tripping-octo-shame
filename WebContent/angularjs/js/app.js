6feb02c7a238339ad29fd507626bb355ada40f58
boriest <boriest@gmail.com>
2015-03-24 13:09:05 +0100

    regzrfzefze

--- WebContent/angularjs/js/app.js
+++ WebContent/angularjs/js/app.js
@@ -1,20 +1,55 @@
 'use strict';
 
+var SERVER_URL = 'http://127.0.0.1:8080/WSProvider/rest/';
+
 /* App Module */
-var phonecatApp = angular.module('produitcatApp', [
+var productApp = angular.module('productApp', [
   'ngRoute',
-  'produitcatControllers'
+  'ngCookies'
 ]);
 
-phonecatApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/produits', {
-        templateUrl: 'partials/ListProduits.html',
-        controller: 'ProduitListCtrl'
-      }).
-       otherwise({
-        redirectTo: '/produits'
-      });
-  }]);
+productApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider
+            .when('/produits', {
+                templateUrl: 'partials/ListProduits.html',
+                controller: 'ProduitListCtrl'
+            })
+            .otherwise({
+                redirectTo: '/produits'
+            });
+    }
+]);
 
+productApp.controller('ProductListCtrl', ['$scope', '$http', '$cookieStore',
+    function ($scope, $http, $cookieStore) {
+        $http.get(SERVER_URL + 'product').success(function(data) {
+            $scope.produits = data;
+        });
+        $scope.orderProp = 'quantity';
+
+        $scope.orderProduct = function (ref, quantity) {
+            console.log(arguments);
+        };
+    }
+]);
+
+productApp.controller('LoginCtrl', ['$scope', '$http', '$cookieStore',
+    function ($scope, $http, $cookieStore) {
+        $scope.handleLogin = function () {
+            var data, headers;
+            data = 'username=' + $scope.username +'&password=' + $scope.password;
+            headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+
+            $http
+                .post(SERVER_URL + 'connect', data, {headers: headers})
+                .success(function (data, status) {
+                    console.log('youpi', data, status);
+                    $cookieStore.put('user_token', data.guid);
+                })
+                .error(function (data, status) {
+                    alert('nope');
+                });
+        };
+    }
+]);
--- WebContent/angularjs/js/controllers.js
+++ WebContent/angularjs/js/controllers.js
@@ -1,13 +1,7 @@
 'use strict';
 
-/* Controllers */
-var produitcatControllers = angular.module('productApp', []);
+var SERVER_URL = 'http://127.0.0.1:8080/WSProvider/rest/';
 
-produitcatControllers.controller('ProductListCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('http://127.0.0.1:8080/WSProvider/rest/product').success(function(data) {
-      $scope.produits = data;
-    });
-    $scope.orderProp = 'quantity';
-  }]);
+/* Controllers */
+var produitcatControllers = angular.module('productApp', ['ngCookies']);
 
